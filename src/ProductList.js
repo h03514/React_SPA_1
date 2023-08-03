4f3e8ff5d242eca8a9d71e35451b26a1bbb9d5bb
MarkChang <h03514@gmail.com>
2023-08-03 17:59:16 +0800

    加入使用useEffect

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,22 +1,29 @@
 import { Link } from "react-router-dom";
 import styles from './ProductList.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Title from "./Title";
 
 export default function ProductList() {
 
-    let porductList = [
-        { "id": 1, "name": "蘋果", "price": 50, "image": "apple.jpg", "description": "新鮮蘋果50克" },
-        { "id": 2, "name": "香蕉", "price": 30, "image": "banana.jpg", "description": "好吃香蕉100克" },
-    ]
 
+    let [porductList, setProductList] = useState([]);
+
+    useEffect(() => {
+        // 1: 無第二個參數 : component 每次 render都會觸發
+        // 2: Dependency Array是空的array時，只會在第一次網頁render時會觸發
+        // 3: Dependency Array有變數時，第一次網頁render時 + 指定的變數改變 會觸發
+        fetch('https://marktoast.com/demoapi/react-basic-product.json')
+            .then(res => res.json())
+            .then(data => setProductList(data))
+            .catch(err => console.log(err));
+    },[]) // <== Dependency Array
     return (
         <div>
-            
+
             <Title mainTitle="請選擇要購買的水果" />
             <div>
                 {
-                   porductList.map((product) => {
+                    porductList.map((product) => {
                         return (
                             <div className={styles.productBorder} key={product.id}>
                                 <p>{product.name}</p>
